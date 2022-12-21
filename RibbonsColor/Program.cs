c4d49cceb77faf6256cb2cb1a2dfab559d593b99
LK <laszlo.kovary@gmail.com>
2022-12-21 10:28:35 +0100

    add logging

--- RibbonsColor/Program.cs
+++ RibbonsColor/Program.cs
@@ -8,17 +8,21 @@ using System.Diagnostics;
 using System.Drawing;
 
 int maxHoles = 100000000;
-Console.WriteLine("Colored Ribbons Solution");
-Stopwatch timer = new Stopwatch();
+Console.WriteLine("Punched out Colored Ribbons Recursive Solution");
+Stopwatch timer = new();
 IGenerateRibbons ribbonGenerator = new GenerateRibbons();
+Console.WriteLine($"Ribbons Model Generate Started (with {maxHoles} holes).");
 List<IRibbonModel> ribbonsModel = ribbonGenerator.Generate(maxHoles);
+Console.WriteLine($"Ribbons Model Generate End (with {maxHoles} holes).");
 // IDisplayContent displayContent = new DisplayContent();
 // displayContent.displayModel(ribbonsModel);
 IColorFinder colorFinder = new ColorFinder();
 timer.Start();
+Console.WriteLine($"Find Colors on Ribbons Started.");
 List<Color> colors = colorFinder.FindColors(ribbonsModel, 0, maxHoles);
+Console.WriteLine($"Find Colors on Ribbons End, {colors.Count} Colors Detected.");
 // IDisplayColors displayColors = new DisplayColors();
 // displayColors.DisplayColorName(colors);
 timer.Stop();
-Console.WriteLine($"Elapsed { timer.Elapsed.ToString() } .");
- 
+Console.WriteLine($"Elapsed {timer.Elapsed} .");
+
